e718a9389320883cbeb2cea7a7b123541ec1f25b
Eliot Swank <eliotswank@gmail.com>
2018-07-06 11:16:09 -0600

    adds test for list of space missions

--- spec/features/visitor_sees_astronauts_spec.rb
+++ spec/features/visitor_sees_astronauts_spec.rb
@@ -18,7 +18,22 @@ describe 'user sees all astronauts' do
 
       visit astronauts_path
 
-      expect(page).to have_content(average_age) 
+      expect(page).to have_content("Average Age:#{average_age}") 
+    end
+    it "sees a list of the space missions in alphabetical order for each astronaut." do
+      astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 30, job: "Commander")
+      astronaut_2 = Astronaut.create(name: "Buzz Aldrin", age: 40, job: "Commander")
+      mission_1 = astronaut_1.space_missions.create(title: "Apollo 13", trip_length: 15 )
+      mission_2 = astronaut_1.space_missions.create(title: "Capricorn 4", trip_length: 20 )
+      mission_3 = astronaut_2.space_missions.create(title: "Capricorn 4", trip_length: 20 )
+      mission_4 = astronaut_2.space_missions.create(title: "Gemini 7", trip_length: 25 )
+
+      visit astronauts_path
+
+      expect(page).to have_content(mission_1.title)
+      expect(page).to have_content(mission_2.title)
+      expect(page).to have_content(mission_3.title)
+      expect(page).to have_content(mission_4.title)
     end
     
   end
@@ -29,10 +44,7 @@ end
 
 
 
-# As a visitor,
-# When I visit '/astronauts'
-# I see the average age of all astronauts.
-# (e.g. "Average Age: 34")
+
 # As a visitor,
 # When I visit '/astronauts'
 # I see a list of the space missions' in alphabetical order for each astronaut.
